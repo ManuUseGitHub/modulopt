07d98e084e531430a00844ecf00868b388da68b5
jean.luc.verhanneman@profectus-it.be <maze014.dev.prod@gmail.com>
2023-11-11 08:24:51 +0100

    build amelioration

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "index.bundle" {
-    export var __esModule: boolean;
-    export function optionize(object: any, optionVector: any, hint: any): any;
-    export function stick(object: any, ...args: any[]): any;
-    export function reset(object: any): void;
-}
